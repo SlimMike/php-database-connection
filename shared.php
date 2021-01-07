4cbe801ce787ad1e23f94662034fda09610a2a67
SlimMike <michal.wezowski@yahoo.com>
2021-01-07 15:28:36 +0100

    explicit disable of persistent connections

--- shared.php
+++ shared.php
@@ -26,7 +26,8 @@ function pdo() : PDO
     $options = [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::ATTR_PERSISTENT         => false
     ];
 
     return new PDO($dsn, $user, $pass, $options);
