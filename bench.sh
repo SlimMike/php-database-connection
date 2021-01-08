0ad041e052e0012a26f6d495ea606f280e76baf9
SlimMike <michal.wezowski@yahoo.com>
2021-01-08 17:09:55 +0100

    try to catch DNS

--- bench.sh
+++ bench.sh
@@ -6,7 +6,14 @@ sleep 5
 
 echo '== Single Query =='
 
-for i in 1 2 3 4 5 6 7 8 9 10
+for i in 1 2 3 4 5
+do
+  php once.php
+done
+
+sleep 6 # public endpoint DNS is cached for 5 or 6 seconds AFAIK
+
+for i in 1 2 3 4 5
 do
   php once.php
 done
